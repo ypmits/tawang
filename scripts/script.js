dad48df41c59255df65d1b0517bd2b7eea312745
Tim Struthoff <tim.struthoff@gmail.com>
2018-07-13 16:04:40 +0200

    init

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+//
+// Available modules include (this is not a complete list):
+// var Scene = require('Scene');
+// var Textures = require('Textures');
+// var Materials = require('Materials');
+// var FaceTracking = require('FaceTracking');
+// var Animation = require('Animation');
+// var Reactive = require('Reactive');
+//
+// Example script
+//
+// Loading required modules
+// var Scene = require('Scene');
+// var FaceTracking = require('FaceTracking');
+//
+// Binding an object's property to a value provided by the face tracker
+// Scene.root.child('object0').transform.rotationY = FaceTracking.face(0).transform.rotationX;
+//
+// If you want to log objects, use the Diagnostics module.
+// var Diagnostics = require('Diagnostics');
+// Diagnostics.log(Scene.root);
