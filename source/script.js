eeb95ff889dd1d2e9624a3f3c1b2be81888aacab
Tim Struthoff <tim.struthoff@mediamonks.com>
2018-07-16 11:59:18 +0200

    clean up

--- source/script.js
+++ source/script.js
@@ -4,8 +4,8 @@ const FaceTracking = require('FaceTracking');
 const Diagnostics = require('Diagnostics');
 const Animation = require('Animation');
 
-Scene.root.child('cube0').transform.rotationX = FaceTracking.face(0).cameraTransform.rotationX.neg();
-Scene.root.child('cube0').transform.rotationY = FaceTracking.face(0).cameraTransform.rotationY;
-Scene.root.child('cube0').transform.rotationZ = FaceTracking.face(0).cameraTransform.rotationZ.neg();
+Scene.root.child('plane0').transform.rotationX = FaceTracking.face(0).cameraTransform.rotationX.neg();
+Scene.root.child('plane0').transform.rotationY = FaceTracking.face(0).cameraTransform.rotationY;
+Scene.root.child('plane0').transform.rotationZ = FaceTracking.face(0).cameraTransform.rotationZ.neg();
 
 
