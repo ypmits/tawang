41a7b44f9a6a0899c9bb27fe6abf61da28d91b37
Tim Struthoff <tim.struthoff@mediamonks.com>
2018-07-16 12:58:45 +0200

    removed junk

--- tools/webpack/webpack.config.js
+++ tools/webpack/webpack.config.js
@@ -1,29 +1,3 @@
-/*const path = require('path');
-const webpack = require('webpack');
-
-
-module.exports = {
-    output: {
-        // path: path.resolve(__dirname, '../../build'),
-        path: __dirname,
-        filename: '[name].[hash].js',
-        publicPath: '/'
-    },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
-    },
-    plugins: []
-};*/
 const webpack = require('webpack');
 const path = require('path');
 
@@ -40,12 +14,4 @@ module.exports = {
         'Reactive': 'commonjs Reactive',
         'Scene': 'commonjs Scene'
     }
-    /*,
-      plugins: [
-        new webpack.IgnorePlugin(/Animation/),
-        new webpack.IgnorePlugin(/Diagnostics/),
-        new webpack.IgnorePlugin(/FaceTracking/),
-        new webpack.IgnorePlugin(/Reactive/),
-        new webpack.IgnorePlugin(/Scene/)
-      ]*/
 };
