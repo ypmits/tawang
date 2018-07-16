0172c97d9bd57c40ee6df700e966b441c11c9ce5
Tim Struthoff <tim.struthoff@gmail.com>
2018-07-16 10:26:14 +0200

    added script functionality

--- scripts/script.js
+++ scripts/script.js
@@ -5,17 +5,33 @@
 // var Materials = require('Materials');
 // var FaceTracking = require('FaceTracking');
 // var Animation = require('Animation');
-// var Reactive = require('Reactive');
+var Reactive = require('Reactive');
 //
 // Example script
 //
 // Loading required modules
-// var Scene = require('Scene');
-// var FaceTracking = require('FaceTracking');
+var Scene = require('Scene');
+var FaceTracking = require('FaceTracking');
 //
 // Binding an object's property to a value provided by the face tracker
-// Scene.root.child('object0').transform.rotationY = FaceTracking.face(0).transform.rotationX;
+var Diagnostics = require('Diagnostics');
+ 
+
+Scene.root.child('cube0').transform.rotationX = FaceTracking.face(0).cameraTransform.rotationX.neg();
+Scene.root.child('cube0').transform.rotationY = FaceTracking.face(0).cameraTransform.rotationY;
+Scene.root.child('cube0').transform.rotationZ = FaceTracking.face(0).cameraTransform.rotationZ.neg();
+
+const Animation = require('Animation');
+
+// animation loops every second
+// var driver = Animation.timeDriver({durationMilliseconds:1000}); 
+// driver.start();
+// Diagnostics.watch("driver", driver); 
+
+// var rotation = FaceTracking.face(0).cameraTransform.rotationX.neg();
+// 
+// Scene.root.child('cube0').transform.rotationX = rotation;
+// Diagnostics.watch("test", rotation); 
 //
 // If you want to log objects, use the Diagnostics module.
-// var Diagnostics = require('Diagnostics');
-// Diagnostics.log(Scene.root);
+
