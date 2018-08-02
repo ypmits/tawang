9cebe7b7fd050e5cbd69301c4b42b0b99679911f
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-02 15:00:55 +0200

    fixed wrong sourcemap generation

--- tools/webpack/webpack.config.js
+++ tools/webpack/webpack.config.js
@@ -1,18 +1,19 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
-    entry: './source/script.js',
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, '../../build'),
-        filename: 'main.bundle.js'
-    },
-    externals: {
-        'Animation': 'commonjs Animation',
-        'Diagnostics': 'commonjs Diagnostics',
-        'FaceTracking': 'commonjs FaceTracking',
-        'Reactive': 'commonjs Reactive',
-        'Scene': 'commonjs Scene'
-    }
+  entry: "./source/script.js",
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "../../build"),
+    filename: "main.bundle.js"
+  },
+  externals: {
+    Animation: "commonjs Animation",
+    Diagnostics: "commonjs Diagnostics",
+    FaceTracking: "commonjs FaceTracking",
+    Reactive: "commonjs Reactive",
+    Scene: "commonjs Scene"
+  }
 };
