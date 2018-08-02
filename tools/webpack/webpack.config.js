4c01192fe16a3655b4d4152ccd0576dd992c2478
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-02 15:05:17 +0200

    updated path

--- tools/webpack/webpack.config.js
+++ tools/webpack/webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
-  entry: "./source/script.js",
+  entry: "./src/script.js",
   mode: "production",
   devtool: "source-map",
   output: {
