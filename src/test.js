5278da176004683365c8271f910c1af8430fca62
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-06 11:56:15 +0200

    clean up

--- src/test.js
+++
@@ -1,9 +0,0 @@
-const Diagnostics = require("Diagnostics");
-
-try {
-  test.test();
-} catch (error) {
-  Diagnostics.log(error);
-  Diagnostics.log(error.message);
-  Diagnostics.log(error.name);
-}
