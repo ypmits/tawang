97c87912b81bc836f8934047f62c348041f62c1a
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-08 10:38:36 +0200

    clean up

--- tools/webpack/index.js
+++
@@ -1,9 +0,0 @@
-const webpack = require("webpack");
-
-const compiler = webpack(require('./webpack.config'));
-
-compiler.run((err, stats) => {
-  console.log('done')
-});
-
-process.stdin.resume();
