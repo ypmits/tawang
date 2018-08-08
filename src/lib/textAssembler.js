b7f601bf99f9440d6ec633952b053d3e10b116b8
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-08 11:49:13 +0200

    Fix path

--- src/lib/textAssembler.js
+++ src/lib/textAssembler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = (options) => {
   let dataJSON = JSON.stringify(options);
-  let wrapperCode = fs.readFileSync('./src/components/wrapperCode.js');
+  let wrapperCode = fs.readFileSync('./src/lib/wrapperCode.js');
   return {
     prepend: `const Diagnostics = require('Diagnostics');\nconst Networking = require('Networking');\ntry {`,
     append: `} catch (error) {\nconst dataJSON = '${dataJSON}'; \n${wrapperCode}\n}`
