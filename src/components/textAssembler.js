3cdf543ee609680de7a33734180b3f9492af9a2a
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-08 10:49:25 +0200

    updated path to reflect directory structure change

--- src/components/textAssembler.js
+++ src/components/textAssembler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = (options) => {
   let dataJSON = JSON.stringify(options);
-  let wrapperCode = fs.readFileSync('./tools/webpack/plugin/components/wrapperCode.js');
+  let wrapperCode = fs.readFileSync('./src/components/wrapperCode.js');
   return {
     prepend: `const Diagnostics = require('Diagnostics');\nconst Networking = require('Networking');\ntry {`,
     append: `} catch (error) {\nconst dataJSON = '${dataJSON}'; \n${wrapperCode}\n}`
