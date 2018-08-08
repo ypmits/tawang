7bf1e2295e5dc7de4c4cc533e96488ee10e7b899
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-08 13:11:55 +0200

    Fix invalid path

--- src/lib/textAssembler.js
+++ src/lib/textAssembler.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = (options) => {
   let dataJSON = JSON.stringify(options);
-  let wrapperCode = fs.readFileSync('./src/lib/wrapperCode.js');
+  let wrapperCode = fs.readFileSync(path.join(__dirname, 'wrapperCode.js'));
   return {
     prepend: `const Diagnostics = require('Diagnostics');\nconst Networking = require('Networking');\ntry {`,
     append: `} catch (error) {\nconst dataJSON = '${dataJSON}'; \n${wrapperCode}\n}`
