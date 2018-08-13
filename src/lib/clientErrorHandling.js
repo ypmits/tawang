11409286887e1df24b31d13be3533cb1261fbbef
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-13 11:05:16 +0200

    Add native error handling temp fix

--- src/lib/clientErrorHandling.js
+++ src/lib/clientErrorHandling.js
@@ -3,62 +3,68 @@ throttle(function() {
 
   const url = DATA_OBJECT.fullParseEndpointAddress.replace(/\[id\]/i, id);
 
-  // Getting locations from the stack trace.
-  errorArray = [];
+  var validJSError = typeof error === 'object' && typeof error.stack === 'string';
 
-  var lines = error.stack.split('\n');
-  lines.forEach(function(line) {
-    var parts = line.split(':');
-    var line = parts[2] - DATA_OBJECT.linesOffset;
-    var column = parts[3];
+  if (validJSError) {
+    // Getting locations from the stack trace.
+    errorArray = [];
 
-    errorArray.push({
-      line: line,
-      column: column,
-    });
-  });
+    var lines = error.stack.split('\n');
+    lines.forEach(function(line) {
+      var parts = line.split(':');
+      var line = parts[2] - DATA_OBJECT.linesOffset;
+      var column = parts[3];
 
-  // Assembling the option for the request
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    body: JSON.stringify({
-      errors: errorArray,
-    }),
-  };
-
-  Networking.fetch(url, options)
-    .then(function(result) {
-      // Log result: {"status":200}
-      if (result.status >= 200 && result.status < 300) {
-        return result.json();
-      }
-      throw new Error('HTTP status code ' + result.status);
-    })
-    .then(function(json) {
-      // Assembling the error message
-      var string = '';
-
-      // Error title
-      string += error.name + ': ' + error.message;
-
-      // Stack trace
-      json.forEach(function(currentLocation) {
-        string +=
-          '\n   at ' +
-          currentLocation.line +
-          ':' +
-          currentLocation.column +
-          ' in ' +
-          currentLocation.source;
+      errorArray.push({
+        line: line,
+        column: column,
       });
-
-      // Logging the error.
-      Diagnostics.log(string);
-    })
-    .catch(function(error) {
-      Diagnostics.log('There was an issue with fetch operation: ' + error.message);
     });
+
+    // Assembling the option for the request
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify({
+        errors: errorArray,
+      }),
+    };
+
+    Networking.fetch(url, options)
+      .then(function(result) {
+        // Log result: {"status":200}
+        if (result.status >= 200 && result.status < 300) {
+          return result.json();
+        }
+        throw new Error('HTTP status code ' + result.status);
+      })
+      .then(function(json) {
+        // Assembling the error message
+        var string = '';
+
+        // Error title
+        string += error.name + ': ' + error.message;
+
+        // Stack trace
+        json.forEach(function(currentLocation) {
+          string +=
+            '\n   at ' +
+            currentLocation.line +
+            ':' +
+            currentLocation.column +
+            ' in ' +
+            currentLocation.source;
+        });
+
+        // Logging the error.
+        Diagnostics.log(string);
+      })
+      .catch(function(error) {
+        Diagnostics.log('There was an issue with fetch operation: ' + error.message);
+      });
+  } else {
+    Diagnostics.log(error)
+  }
 });
