dc1687bec3a136880c892497880b0ddaaafedb95
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-22 15:16:54 +0200

    Improve test

--- test/Validator.test.js
+++ test/Validator.test.js
@@ -6,34 +6,44 @@ describe('Validator', () => {
   const Validator = require('../src/lib/Validator');
   let validator = new Validator();
   describe('#sourceMapEndpoint', () => {
-    it('should return a string when no string is supplied', () => {
-      expect(validator.sourceMapEndpoint()).to.be.a('string');
+    describe('with no arguments', () => {
+      it('should return a string', () => {
+        expect(validator.sourceMapEndpoint()).to.be.a('string');
+      });
     });
 
-    it('should return a path starting with /', () => {
-      expect(validator.sourceMapEndpoint('source-map/')).to.startWith('/');
+    describe('with "souce-map/"', () => {
+      it('should return a path starting with /', () => {
+        expect(validator.sourceMapEndpoint('source-map/')).to.startWith('/');
+      });
     });
   });
 
   describe('#parseEndpoint', () => {
-    it('should return a string when no string is supplied', () => {
-      expect(validator.parseEndpoint()).to.be.a('string');
+    describe('with no arguments', () => {
+      it('should return a string', () => {
+        expect(validator.parseEndpoint()).to.be.a('string');
+      });
     });
 
-    it('should return a path starting with /', () => {
-      expect(validator.parseEndpoint('source-map/[id]')).to.startWith('/');
+    describe('with source-map/[id]', () => {
+      it('should return a path starting with /', () => {
+        expect(validator.parseEndpoint('source-map/[id]')).to.startWith('/');
+      });
+
+      it('should include the placeholder', () => {
+        expect(validator.parseEndpoint('source-map/[id]')).to.include('[id]');
+      });
     });
 
-    it('should include the placeholder', () => {
-      expect(validator.parseEndpoint('source-map/[id]')).to.include('[id]');
-    });
-
-    it('should throw an error when there is https in the url', () => {
-      expect(
-        validator.parseEndpoint.bind(validator, 'https://example.com/source-map/[id]'),
-      ).to.throw(
-        'Tawang: getEndPoint must not be a full address. Please provide only the relative address to the serverHost (e.g. /sourcemap)',
-      );
+    describe('with https://example.com/source-map/[id]', () => {
+      it('should throw an error', () => {
+        expect(
+          validator.parseEndpoint.bind(validator, 'https://example.com/source-map/[id]'),
+        ).to.throw(
+          'Tawang: getEndPoint must not be a full address. Please provide only the relative address to the serverHost (e.g. /sourcemap)',
+        );
+      });
     });
   });
 });
