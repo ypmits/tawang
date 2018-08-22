930b9db212917d574fe2fbe48cc7f0d23abcda51
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-22 18:14:40 +0200

    Add more tests

--- test/Tawang.test.js
+++ test/Tawang.test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const { expect } = chai;
+chai.use(require('chai-as-promised'));
 
 const MemoryFileSystem = require('memory-fs'); // eslint-disable-line import/no-extraneous-dependencies
 const webpack = require('webpack');
@@ -57,6 +58,7 @@ describe('Tawang', () => {
       });
     });
   });
+
   describe('with valid options in development mode', () => {
     it('should run successfully', done => {
       const path = require('path');
@@ -108,6 +110,112 @@ describe('Tawang', () => {
         // console.log(stats);
         done();
       });
+    });
+  });
+
+  describe('with no options in development mode', () => {
+    it('should throw an error', async () => {
+      await expect(
+        new Promise(() => {
+          const path = require('path');
+          const Tawang = require('./../');
+
+          const compiler = webpack({
+            entry: path.resolve(__dirname, './mock-data/Tawang/src/script.js'),
+            output: {
+              path: '/build',
+              filename: 'main.bundle.js',
+            },
+            plugins: [new Tawang()],
+            mode: 'development',
+            devtool: 'source-map',
+          });
+          let files = new MemoryFileSystem();
+          compiler.outputFileSystem = files;
+          compiler.run();
+        }),
+      ).to.be.rejectedWith('options');
+    });
+  });
+
+  describe('with no options in production mode', () => {
+    it('should throw an error', async () => {
+      await expect(
+        new Promise(() => {
+          const path = require('path');
+          const Tawang = require('./../');
+
+          const compiler = webpack({
+            entry: path.resolve(__dirname, './mock-data/Tawang/src/script.js'),
+            output: {
+              path: '/build',
+              filename: 'main.bundle.js',
+            },
+            plugins: [new Tawang()],
+            mode: 'production',
+          });
+          let files = new MemoryFileSystem();
+          compiler.outputFileSystem = files;
+          compiler.run();
+        }),
+      ).to.be.rejectedWith('options');
+    });
+  });
+
+  describe('with no serverHost option in production mode', () => {
+    it('should throw a serverHost error', async () => {
+      await expect(
+        new Promise(() => {
+          const path = require('path');
+          const Tawang = require('./../');
+
+          const compiler = webpack({
+            entry: path.resolve(__dirname, './mock-data/Tawang/src/script.js'),
+            output: {
+              path: '/build',
+              filename: 'main.bundle.js',
+            },
+            plugins: [
+              new Tawang({
+                serverHost: null,
+              }),
+            ],
+            mode: 'production',
+          });
+          let files = new MemoryFileSystem();
+          compiler.outputFileSystem = files;
+          compiler.run();
+        }),
+      ).to.be.rejectedWith('serverHost');
+    });
+  });
+
+  describe('with no serverHost option in development mode', () => {
+    it('should throw a serverHost error', async () => {
+      await expect(
+        new Promise(() => {
+          const path = require('path');
+          const Tawang = require('./../');
+
+          const compiler = webpack({
+            entry: path.resolve(__dirname, './mock-data/Tawang/src/script.js'),
+            output: {
+              path: '/build',
+              filename: 'main.bundle.js',
+            },
+            plugins: [
+              new Tawang({
+                serverHost: null,
+              }),
+            ],
+            mode: 'development',
+            devtool: 'source-map',
+          });
+          let files = new MemoryFileSystem();
+          compiler.outputFileSystem = files;
+          compiler.run();
+        }),
+      ).to.be.rejectedWith('serverHost');
     });
   });
 });
